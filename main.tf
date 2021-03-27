d550d2ea95beb895039d741fa97e6ef196a9462e
DariuSGB <DariuSGB@github.com>
2021-03-27 20:26:47 +0100

    finished section 4

--- main.tf
+++ main.tf
@@ -15,6 +15,7 @@ variable avail_zone1 {}
 variable avail_zone2 {}
 variable env_prefix {}
 variable instance_type {}
+variable public_key_location {}
 
 # Defining a VPC
 
@@ -176,12 +177,37 @@ data "aws_ami" "lastest-ami" {
 ## Print all the variable of the most recent Amanzon Machine Image
 
 output "aws_ami_id" {
-    value = data.aws_ami.lastest-ami
+    value = data.aws_ami.lastest-ami.id
 }
+
+output "ec2_public_ip" {
+    value = aws_instance.myapp-server.public_ip
+}
+
+resource "aws_key_pair" "ssh-key" {
+    key_name = "server-key"
+    public_key = file(var.public_key_location)
+}
+
 
 ## Defining an instance
 
 resource "aws_instance" "myapp-server" {
+    # Defining Server instance
     ami = data.aws_ami.lastest-ami.id
     instance_type = var.instance_type
+    # Defining subnet, SG and AZ
+    subnet_id = aws_subnet.myapp-subnet-1.id
+    vpc_security_group_ids = [aws_default_security_group.main-sg.id]
+    availability_zone = var.avail_zone1
+    # Defining public IP
+    associate_public_ip_address = true
+    # Defining SSH keys
+    key_name = aws_key_pair.ssh-key.key_name
+    # Executing a BASH script
+    user_data = file("entry-script.sh")
+
+    tags = {
+        Name: "${var.env_prefix}-server"
+    }
 }
